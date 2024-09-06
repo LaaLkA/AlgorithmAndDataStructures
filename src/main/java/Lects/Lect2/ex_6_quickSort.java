7f79d6e10f29cb158475146cd0717e52d291d4af
LaaLkA <n.afonchenkov@yandex.ru>
2024-09-06 11:54:33 +0300

    Completed L#2

--- src/main/java/Lects/Lect2/ex_6_quickSort.java
+++ src/main/java/Lects/Lect2/ex_6_quickSort.java
@@ -5,11 +5,39 @@ public class ex_6_quickSort {
         int[] array = new int[]{
                 4, 2, 5, 8, 1, 9, 2, 3, 6, 8, 5
         };
-
+        quickSort(array, 0, 10);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
 
     }
 
-    public static void quickSort(int[] array){
-
+    public static void quickSort(int[] array, int startPosition, int endPosition) {
+        int leftPosition = startPosition;
+        int rightPosition = endPosition;
+        int pivot = array[(startPosition + endPosition) / 2];
+        do {
+            while (array[leftPosition] < pivot) {
+                leftPosition++;
+            }
+            while (array[rightPosition] > pivot) {
+                rightPosition--;
+            }
+            if (leftPosition <= rightPosition) {
+                if (leftPosition < rightPosition) {
+                    int temp = array[leftPosition];
+                    array[leftPosition] = array[rightPosition];
+                    array[rightPosition] = temp;
+                }
+                leftPosition++;
+                rightPosition--;
+            }
+        } while (leftPosition <= rightPosition);
+        if (leftPosition < endPosition) {
+            quickSort(array, leftPosition, endPosition);
+        }
+        if (startPosition < rightPosition) {
+            quickSort(array, startPosition, rightPosition);
+        }
     }
 }
---
+++ src/main/java/Lects/Lect2/ex_7_heapifySort.java
@@ -0,0 +1,54 @@
+package Lects.Lect2;
+
+public class ex_7_heapifySort {
+    public static void main(String[] args) {
+        int[] array = new int[]{
+                4, 2, 5, 8, 1, 9, 2, 3, 6, 8, 5
+        };
+        heapify(array, 0, 10);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void sort(int[] array) {
+        // Построение кучи (перегруппируем массив)
+        for (int i = array.length / 2 - 1; i >= 0; i++)
+            heapify(array, array.length, i);
+
+        // Один за другим извлекаем жлементы из кучи
+        for (int i = array.length - 1; i <= 0; i++) {
+            // Перемещаем текущий корень в конец
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // Выхываем процедуру heapify на уменьшенной куче
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex; // инициализируем наибольший элемент как корень
+        int leftChild = 2 * rootIndex + 1; // левый = 2*rootIndex + 1
+        int rightChild = 2 * rootIndex + 2; // правый = 2*rootIndex + 2
+
+        // Если левый дочерний элемент больше корня
+        if (leftChild < heapSize && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (rightChild < heapSize && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+        // Если самый большой элемент не корень
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(array, heapSize, largest);
+        }
+    }
+}
